0629c1a7e8ea98f50a6e9f673655e2658152a534
Tyler Thompson <tyler@tylerpaulthompson.com>
2016-01-20 13:23:31 -0800

    Separate cell style resolution logic from Cell

--- src/components/cell.js
+++ src/components/cell.js
@@ -1,61 +1,16 @@
 import React, { PropTypes } from 'react';
 import Radium from 'radium';
+import resolveCellStyles from './util/resolve-cell-styles';
+import omit from 'lodash.omit';
 
 const Cell = Radium(props => {
-  // Translate grid-speak to flexbox-speak
-  const alignmentMap = {
-    horizontal: {
-      left: 'flex-start',
-      center: 'center',
-      right: 'flex-end'
-    },
-    vertical: {
-      top: 'flex-start',
-      center: 'center',
-      bottom: 'flex-end'
-    }
-  };
+  const cellOverrides = omit(props, 'children');
 
-  const { width, alignment } = props;
-
-  // Generate the style object. Each breakpoint
-  // uses its media query as its key and
-  // breakpoint-specific styles as its values.
-  // Radium handles the actual breakpoint transitions.
-  const styles = Object.keys(
-    props.breakpoints
-  ).reduce((acc, breakpoint) => {
-    // Extract the media query and the breakpoint's specific cell config
-    const breakpointSettings = props.breakpoints[breakpoint];
-    const { mediaQuery, cells: breakpointDefaultCells } = breakpointSettings;
-
-    // Determine the final cell configuration.
-    // In order of precedence:
-    // - this cell's width and alignment props
-    // - the breakpoint's cell defaults
-    // - the grid's cell defaults
-    const cellConfig = {
-      ...props.defaultCells,
-      ...breakpointDefaultCells,
-      ...{ width, alignment }
-    };
-
-    return {
-      ...acc,
-      [mediaQuery]: {
-        flexBasis: `${cellConfig.width * 100}%`,
-        alignSelf: alignmentMap.vertical[
-          cellConfig.alignment.vertical
-        ],
-        justifyContent: alignmentMap.horizontal[
-          cellConfig.alignment.horizontal
-        ],
-        order: cellConfig.order
-      }
-    };
-  }, {
-    ...props.style,
-    display: 'flex'
+  const styles = resolveCellStyles({
+    breakpoints: props.breakpoints,
+    cellOverrides: cellOverrides,
+    gridDefaultCells: props.defaultCells,
+    parentStyles: props.style
   });
 
   return (
@@ -73,14 +28,6 @@ Cell.propTypes = {
   children: React.PropTypes.node,
   order: PropTypes.number,
   width: PropTypes.number
-};
-
-Cell.defaultProps = {
-  alignment: {
-    horizontal: 'left',
-    vertical: 'top'
-  },
-  width: 1 / 3
 };
 
 export default Cell;
