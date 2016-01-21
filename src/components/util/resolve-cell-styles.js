9df44a809caf5d96a9388a55bc5985d55adeae8d
Tyler Thompson <tyler@tylerpaulthompson.com>
2016-01-20 16:22:26 -0800

    Refactor the Grid and Cell props to be more JSX-friendly (and user-friendly)

--- src/components/util/resolve-cell-styles.js
+++ src/components/util/resolve-cell-styles.js
@@ -1,6 +1,6 @@
-const resolveCellStyles = ({
-  breakpoints, cellOverrides, gridDefaultCells, parentStyles
-}) => {
+import merge from 'lodash.merge';
+
+const resolveCellStyles = props => {
   // Translate grid-speak to flexbox-speak
   const alignmentMap = {
     horizontal: {
@@ -15,38 +15,71 @@ const resolveCellStyles = ({
     }
   };
 
+  const gridDefault = {
+    width: props.cellWidth,
+    horizontalAlign: props.cellAlign,
+    verticalAlign: props.cellVerticalAlign
+  };
+
+  const cellDefault = {
+    width: props.width,
+    horizontalAlign: props.horizontalAlign,
+    verticalAlign: props.verticalAlign
+  };
+
+  const breakpoints = ['small', 'medium', 'large', 'xlarge'].map(size => {
+    return {
+      mediaQuery: props.breakpoints[size],
+      gridBreakpointDefault: {
+        width: props[`${size}CellWidth`],
+        horizontalAlign: props[`${size}CellAlign`],
+        verticalAlign: props[`${size}CellVerticalAlign`]
+      },
+      cellBreakpointDefault: {
+        width: props[`${size}Width`],
+        horizontalAlign: props[`${size}Align`],
+        verticalAlign: props[`${size}VerticalAlign`]
+      }
+    };
+  });
+
   return Object.keys(breakpoints).reduce((acc, breakpoint) => {
-    // Extract the media query and the breakpoint's specific cell config
-    const breakpointMap = breakpoints[breakpoint];
-    const { mediaQuery, cells: breakpointDefaultCells } = breakpointMap;
+    // Extract the media query and the breakpoint cell configs
+    const {
+      mediaQuery,
+      gridBreakpointDefault,
+      cellBreakpointDefault
+    } = breakpoints[breakpoint];
 
     // Determine the final cell configuration.
-    // In order of precedence:
-    // - this cell's width and alignment props
-    // - the breakpoint's cell defaults
-    // - the grid's cell defaults
-    const cellConfig = {
-      ...gridDefaultCells,
-      ...breakpointDefaultCells,
-      ...cellOverrides
-    };
+    // Uses these sources for cell styles, in order of precedence:
+    // - Grid default
+    // - Grid breakpoint default
+    // - Cell default
+    // - Cell breakpoint default
+    const cellConfig = merge({},
+      gridDefault,
+      gridBreakpointDefault,
+      cellDefault,
+      cellBreakpointDefault
+    );
 
     return {
       ...acc,
       [mediaQuery]: {
         flexBasis: `${cellConfig.width * 100}%`,
         alignSelf: alignmentMap.vertical[
-          cellConfig.alignment.vertical
+          cellConfig.verticalAlign
         ],
         justifyContent: alignmentMap.horizontal[
-          cellConfig.alignment.horizontal
+          cellConfig.horizontalAlign
         ],
         order: cellConfig.order ? cellConfig.order : 'initial'
       }
     };
   }, {
-    ...parentStyles,
-    display: 'flex'
+    display: 'flex',
+    ...props.styles
   });
 };
 
