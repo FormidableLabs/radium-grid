660eeda86bab40fad88df9df005f344efbe51eae
Formidable <Formidable@Formidables-MacBook-Pro.local>
2016-02-05 17:22:38 -0800

    fix additional linting errors, refactor parse-fraction.js

--- src/components/util/parse-fraction.js
+++ src/components/util/parse-fraction.js
@@ -1,33 +1,43 @@
-const parseFraction = (string) => {
-  const [n, d] = string.split("/");
+const analyzeInput = (string) => {
+  const [n, d] =  string.split("/");
   const numerator = parseInt(n, 10);
   const denominator = parseInt(d, 10);
+  const zeroIndex = 0;
+  const result = numerator / denominator;
 
-  if (string.indexOf(" ") > -1) {
+  if (string.indexOf(" ") >= zeroIndex) {
     throw new Error("Your fraction must not contain any spaces.");
-  }
-  if (numerator === 1 && (!denominator && isNaN(denominator))) {
-    return numerator;
   }
 
   if (denominator === 0) {
     throw new Error("The fraction you provided divides by zero.");
   }
 
-  if (!(numerator && denominator)) {
-    throw new Error("Your fraction is missing a numerator or denominator.");
+  if ((!numerator && isNaN(numerator)) || (!denominator && isNaN(denominator))) {
+    if (numerator !== 1) { 
+      throw new Error("Your fraction is missing a numerator or denominator.");
+    }
+  }
+
+  if (result > 1) {
+    throw new Error("Your fraction must be less than or equal to 1.");
+  }
+
+  return [numerator, denominator];
+};
+
+const parseFraction = (string) => {
+  const [numerator, denominator] = analyzeInput(string);
+
+  if (numerator === 1 && (!denominator && isNaN(denominator))) {
+    return numerator;
   }
 
   if (numerator === denominator) {
     return 1;
   }
 
-
-  const result = numerator / denominator;
-  if (result > 1) {
-    throw new Error("Your fraction must be less than or equal to 1.");
-  }
-  return result;
+  return numerator / denominator;
 };
 
 export default parseFraction;
