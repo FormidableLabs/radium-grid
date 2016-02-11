f2885f1dd4f6ab4dfab3b770c97f51404c3086f1
Tyler Thompson <tyler@tylerpaulthompson.com>
2016-02-11 12:52:10 -0800

    Horrible hack to allow only grid/cell media queries to be sorted

---
+++ src/components/style-root.js
@@ -0,0 +1,30 @@
+/* eslint-disable new-cap */
+import Enhancer from "radium/lib/enhancer";
+import StyleRoot from "radium/lib/components/style-root";
+// import StyleSheet from "radium/lib/style-sheet";
+
+import StyleKeeper from "./util/style-keeper";
+
+const _getStyleKeeper = (instance) => {
+  if (!instance._radiumStyleKeeper) {
+    const userAgent = (
+      instance.props.radiumConfig && instance.props.radiumConfig.userAgent
+    ) || (
+      instance.context._radiumConfig && instance.context._radiumConfig.userAgent
+    );
+    instance._radiumStyleKeeper = new StyleKeeper(userAgent);
+  }
+
+  return instance._radiumStyleKeeper;
+};
+
+class GridStyleRoot extends StyleRoot {
+  constructor() {
+    super(...arguments);
+
+    this._radiumStyleKeeper = null;
+    _getStyleKeeper(this);
+  }
+}
+
+export default Enhancer(GridStyleRoot);
---
+++ src/components/util/style-keeper.js
@@ -0,0 +1,43 @@
+import StyleKeeper from "radium/lib/style-keeper";
+import contains from "lodash.contains";
+import sortMediaQueries from "sort-media-queries";
+
+export default class GridStyleKeeper extends StyleKeeper {
+  constructor(userAgent: string) {
+    super(userAgent);
+    this._cascadingCSS = [];
+  }
+
+  addCSS(css: string): {remove: () => void} {
+    const cascade = css.indexOf("[CASCADE]") !== -1;
+    if (cascade) {
+      const cleanRule = css.replace(/\[CASCADE\]/g, "");
+      if (!contains(this._cascadingCSS, cleanRule)) {
+        this._cascadingCSS.push(cleanRule);
+        this._emitChange();
+      }
+    } else if (!this._cssSet[css]) {
+      this._cssSet[css] = true;
+      this._emitChange();
+    }
+
+    return {
+      // Must be fat arrow to capture `this`
+      remove: () => {
+        if (cascade) {
+          const index = this._cascadingCSS.indexOf(css);
+          this._cascadingCSS.splice(index, 1);
+        } else {
+          delete this._cssSet[css];
+        }
+        this._emitChange();
+      }
+    };
+  }
+
+  getCSS(): string {
+    return Object.keys(this._cssSet)
+      .concat(sortMediaQueries(this._cascadingCSS))
+      .join("\n");
+  }
+}
